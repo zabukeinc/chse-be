4500f66c0db2e0e03f3b836c0721e5463ed02070
Miftah Regyana <miftahregyana@gmail.com>
2022-04-03 09:54:21 +0700

    feat(supplier): add code generator when create data

--- src/app/supporting/supplier/domain/usecases/managers/create-supplier.manager.ts
+++ src/app/supporting/supplier/domain/usecases/managers/create-supplier.manager.ts
@@ -1,4 +1,5 @@
 import { BaseCreateManager } from "src/app/base/domain/usecases/managers/base-create.manager";
+import { CodeGeneratorHelper } from "src/helpers/code-generator.helper";
 import { SupplierDataService } from "../../../data/services/supplier-data.service";
 import { SupplierEntity } from "../../entities/supplier.entity";
 
@@ -15,6 +16,9 @@ export class CreateSupplierManager extends BaseCreateManager<SupplierEntity> {
   }
 
   async prepareData(): Promise<SupplierEntity> {
+    this.entity.code = await new CodeGeneratorHelper(
+      this.service, this.entity
+    ).setPrefix('SUP').generate()
     return this.entity
   }
 }
