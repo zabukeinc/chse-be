9fdb39d0c36c4ac71eea7484979b31f4f7fadfb6
Miftah Regyana <miftahregyana@gmail.com>
2022-04-13 23:06:56 +0700

    fix(document): fixing search

--- src/app/supporting/document/domain/usecases/document.orchestrator.ts
+++ src/app/supporting/document/domain/usecases/document.orchestrator.ts
@@ -25,7 +25,6 @@ export class DocumentOrchestrator extends BaseOrchestrator<DocumentEntity>{
       Object.assign(params.where, {
         ...params.where as Object,
         title: Like(`%${search}%`),
-        name: Like(`%${search}%`)
       });
     }
 
